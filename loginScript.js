e7dce20e93307e902dcee92a826dffd5c0b3455a
Theo Woodward <theodore.woodward@students.plymouth.ac.uk>
2020-02-13 14:48:36 +0000

    Register form now checks username is unique

--- loginScript.js
+++ loginScript.js
@@ -1,4 +1,6 @@
-function startListeningCaps(){
+const e = require("express");
+
+function startListeningCaps() {
     input.addEventListener("keyup", function (event) {
         if (event.getModifierState("CapsLock")) {
             text.style.display = "block";
@@ -7,10 +9,10 @@ function startListeningCaps(){
             text.style.display = "none"
         }
     });
-  }
+}
 
-  function startListeningEnter(){
-          // Execute a function when the user releases a key on the keyboard
+function startListeningEnter() {
+    // Execute a function when the user releases a key on the keyboard
     input.addEventListener("keyup", function (event) {
         // Number 13 is the "Enter" key on the keyboard
         if (event.keyCode === 13) {
@@ -20,62 +22,70 @@ function startListeningCaps(){
             loginButton();
         }
     });
-  }
+}
 
 
-  function getUserSalt(username, data){
-    for(i = 0; i<result.length; i++){
-        if(result[i].name == username){
+function getUserSalt(username, data) {
+    for (i = 0; i < result.length; i++) {
+        if (result[i].name == username) {
             return result[i].salt;
         }
     }
-  }
+}
 
-  function loginButton() {
-    console.log("Login button pressed");
+function checkUserUnique(username, data){
+    for (i = 0; i < result.length; i++) {
+        if (result[i].name == username) {
+            return false;
+        }
+    }
+}
 
-    //Get username and password values entered by user
+function loginButton() {
+    //Get username entered
     var userName = $("#txtUserName").val();
+    //Get password entered
     var password = $("#txtPassword").val();
-
-    //Need to get correct salt, not just first in list
+    //Find correct user's salt in database
     var salt = getUserSalt(userName, result);
-    //Now hash the password
+    //Now hash the password with the correct salt
     var hashPass = new Hashes.MD5().hex(password + salt);
-    //hashPass = "hello";
     var k; //k is used in loop below to find user
     var wrongPassword = true; //Set password as wrong by default
     console.log(hashPass);
-    for (k in result) {
-        if (userName == result[k].name && hashPass == result[k].password) {
+    for (k in result) { //For each user in the database
+        if (userName == result[k].name && hashPass == result[k].password) { //Check if the username and password match up
             sessionStorage.setItem('loggedIn', 'true');
             sessionStorage.setItem('role', result[k].role);
             wrongPassword = false;
             //Go to main page
             document.location.assign("index.html");
         }
-
     }
-    if(wrongPassword == true){
+    if (wrongPassword == true) {
         $('#alertLogin').show();
     }
 }
 
-
-
-        
-function generateSalt(){
+//Generates a random salt
+function generateSalt() {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  }
+}
 
-  function userRegistered(){
-    var salt = generateSalt();
-    var password = document.getElementById("txtPassword").value;
-    var passwordSalt = password+salt;
-    var hashPass = new Hashes.MD5().hex(password + salt); //Hash password for security
-    $("#txtPassword").val(hashPass.toString());
-    $("<input />").attr("type", "hidden")
-          .attr("name", "salt")
-          .attr("value", salt.toString())
-          .appendTo("#registerForm");
-  }
+function userRegistered() {
+    if (checkUserUnique()){
+        var salt = generateSalt(); //Generate random salt
+        var password = $("#txtPassword").val(); //Get password
+        var passwordSalt = password + salt;
+        var hashPass = new Hashes.MD5().hex(password + salt); //Hash password for security
+        $("#txtPassword").val(hashPass.toString()); //Store hashed password on form
+        $("<input />").attr("type", "hidden") //Store salt on form
+            .attr("name", "salt")
+            .attr("value", salt.toString())
+            .appendTo("#registerForm");
+    }
+    else{
+        document.location.assign("login.html");
+    }
+
+}
