4f62cd51f231049947d7c9ec405cff2d0c79104c
Theo Woodward <theodore.woodward@students.plymouth.ac.uk>
2020-03-17 13:30:04 +0000

    Refactored code on login page

--- libraries/loginScript.js
+++ libraries/loginScript.js
@@ -1,13 +1,12 @@
-
-function startListeningEnter() {
-    // Execute a function when the user releases a key on the keyboard
-    input.addEventListener("keyup", function (event) {
+//Listens for the enter key on specified element. If pressed, executes specified function
+function startListeningEnter(element, func) {
+    element.addEventListener("keyup", function (event) {
         // Number 13 is the "Enter" key on the keyboard
         if (event.keyCode === 13) {
             // Cancel the default action, if needed
             event.preventDefault();
             // Trigger the button element with a click
-            loginButton();
+            func();
         }
     });
 }
