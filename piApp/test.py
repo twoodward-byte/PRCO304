1ef0018567b80c4d4329d7c9acfc790ddb0f0fb5
Theodore Woodward <theodore.woodward@students.plymouth.ac.uk>
2020-04-06 11:06:14 +0100

    Simulate button press now linked to the database, adds a sample record

--- piApp/test.py
+++ piApp/test.py
@@ -5,13 +5,13 @@ from pymongo import MongoClient
 from pprint import pprint
 from datetime import datetime
 
-datetime_now = datetime.now() # pass this to a MongoDB doc
 client = MongoClient("mongodb+srv://test:test@cluster0-bcfvz.mongodb.net/test?retryWrites=true&w=majority")
 db = client["FinalProject"]
 mycol = db["partsProduced"]
 
 root = tk.Tk()
 root.title("Simulate Button Press")
+
 #Create label widget
 myLabel = Label(root, text="Sensor set to: Sidewall #323")
 #Attach label to screen
@@ -21,9 +21,12 @@ myLabel.pack()
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42") 
 canvas.pack()
 
-def testFunc():
-	print("asdasd")
-	
+def testFunc(): #Executes when button pressed
+	datetime_now = datetime.now() # pass this to a MongoDB doc
+	print("Simulate button pressed")
+	mydict = {"amount":"123", "name":"piTest", "date":datetime_now}
+	x = mycol.insert_one(mydict)
+
 	
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
