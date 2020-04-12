dee9ec694f36d377d2ef323dfcf029d74bda3bae
Theodore Woodward <theodore.woodward@students.plymouth.ac.uk>
2020-04-12 13:00:00 +0100

    pressing start now generates a random record once

--- randomDataApp/randomTestingData.py
+++ randomDataApp/randomTestingData.py
@@ -4,6 +4,8 @@ import os
 from pymongo import MongoClient
 from pprint import pprint
 from datetime import datetime
+import random
+import time
 
 
 #Set mongo db cloud URI
@@ -27,16 +29,22 @@ def btnStopClick():
 
 def btnStartClick():
 	#random number between 1 and 10 for amount
+	x = random.randint(1, 10)
 	#random part picked
+	
+	#get parts from database in dictionary
+	#use random number to pick one as an index
+	#make a variable equal to this
+	
+	
 	#insert this into database with pi as origin
-	#pick random number between 1 and 10 
-	#wait this long before executing again.
-	
-	
 	datetime_now = datetime.now() # pass this to a MongoDB doc
-	print("Simulate button pressed")
-	mydict = {"amount":"1", "name":"Bracket #423", "date":datetime_now, "origin": "Pi"}
+	mydict = {"amount":x, "name":"Bracket #423", "date":datetime_now, "origin": "Pi"}
 	x = mycol.insert_one(mydict)
+	#pick random number between 1 and 10 for seconds to sleep
+	x = random.randint(1, 10)
+	#wait this long before executing again.
+	time.sleep(x)
 	
 #create canvas
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42") 
